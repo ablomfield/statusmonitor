19bb0c112e6eb66fba3373ef418492bb8f154569
Adam Blomfield <adam@blomfield.me>
2025-07-22 17:51:50 -0500

    header

--- html/includes/header.php
+++ html/includes/header.php
@@ -1,4 +1,4 @@
 				<div id="header">
 					<!-- Logo -->
-					<h1><a href="/"><img height="75px" src="/images/icononly_transparent_nobuffer.png" alt="CollabToolbox"></a>&nbsp;&nbsp;&nbsp;<a href="/" id="logo"><?php echo($sitetitle); ?></a></h1>
+					<h1><a href="/"><img height="100px" src="/images/icononly_transparent_nobuffer.png" alt="CollabToolbox"></a>&nbsp;&nbsp;&nbsp;<a href="/" id="logo"><?php echo($sitetitle); ?></a></h1>
 				</div>
