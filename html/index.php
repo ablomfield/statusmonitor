d725cfb3a6cd7b5e5bcd9fbee19b4ab5d3e31241
Adam Blomfield <adam@blomfield.me>
2025-07-22 17:53:13 -0500

    icon

--- html/index.php
+++ html/index.php
@@ -13,7 +13,7 @@ include($_SERVER['DOCUMENT_ROOT'] . "/includes/settings.php");
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="/assets/css/main.css" />
-  		<link rel="icon" type="image/x-icon" href="/images/icononly_transparent_nobuffer.png">
+  		<link rel="icon" type="image/x-icon" href="/images/icon.png">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
 	</head>
 	<body class="is-preload">
