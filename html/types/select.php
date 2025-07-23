8e18f743dfa3d2da4c3b25e8145636819eaf2952
Adam Blomfield <adam@blomfield.me>
2025-07-22 20:01:23 -0500

    select

--- html/types/select.php
+++ html/types/select.php
@@ -1,5 +1,5 @@
 <?php
-echo ("			<form method=\"post\" action=\"select\">\n");
+echo ("			<form method=\"post\" action=\"select/\">\n");
 echo ("			  <input type=\"hidden\" name=\"type\" value=\"lgw\">\n");
 echo ("			  <input type=\"submit\" value=\"Local Gateways\">\n");
 echo ("			<form method=\"post\" action=\"select\">\n");
