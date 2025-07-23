fcb373086246c44685bab337b5706540831a1dab
Adam Blomfield <adam@blomfield.me>
2025-07-22 20:02:49 -0500

    select

--- html/select/index.php
+++ html/select/index.php
@@ -2,8 +2,9 @@
 
 if (isset($_POST["type"])) {
     $devtype = $_POST["type"];
+    echo ("\$devtype = $devtype\n<br>");
     $_SESSION["devtype"] = $devtype;
-    header("Location: /");
+    //header("Location: /");
 } else {
     header("Location: /");
 }
