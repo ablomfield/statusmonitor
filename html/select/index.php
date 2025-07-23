27ca2d789fc5c0ee55a507c444dcc8d0d3e16f0b
Adam Blomfield <adam@blomfield.me>
2025-07-22 20:04:33 -0500

    devtype

--- html/select/index.php
+++ html/select/index.php
@@ -1,10 +1,10 @@
 <?php
-
+session_start();
 if (isset($_POST["type"])) {
     $devtype = $_POST["type"];
     echo ("\$devtype = $devtype\n<br>");
     $_SESSION["devtype"] = $devtype;
-    //header("Location: /");
+    header("Location: /");
 } else {
     header("Location: /");
 }
