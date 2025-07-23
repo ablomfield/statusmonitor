2a8a4e15b02a5602b490b87dc66cdc82c5893c22
Adam Blomfield <adam@blomfield.me>
2025-07-23 10:14:18 -0500

    lgw

--- html/types/lgw.php
+++ html/types/lgw.php
@@ -20,9 +20,30 @@ if (curl_errno($gettrunks) == "0") {
     $trunkcount = count($trunksarray['trunks']);
     echo ("					    <table class=\"default\">\n");
     for ($x = 0; $x < $trunkcount; $x++) {
+        // Retrieve Trunk Information
+        $trunkid = $trunksjson->trunks[$x]->id;
+        $trunkdeturl = "https://webexapis.com/v1/telephony/config/premisePstn/trunks/$trunkid?orgId=$orgid";
+        $gettrunkdet = curl_init($trunkdeturl);
+        curl_setopt($gettrunkdet, CURLOPT_CUSTOMREQUEST, "GET");
+        curl_setopt($gettrunkdet, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt(
+            $gettrunkdet,
+            CURLOPT_HTTPHEADER,
+            array(
+                'Content-Type: application/json',
+                'Authorization: Bearer ' . $authtoken
+            )
+        );
+        $trunkdetdata = curl_exec($gettrunkdet);
+        $trunkdetjson = json_decode($trunkdetdata);
+        if ($trunkdetjson->status == "online") {
+            $statusimg = "/images/status-green.png";
+        } else {
+            $statusimg = "/images/status-red.png";
+        }
         echo ("					      <tr>\n");
         echo ("					        <td>\n");
-        echo ("     					    <img src=\"/images/status-green.png\">\n");
+        echo ("     					    <img src=\"" . $statusimg . "\">\n");
         echo ("					        </td>\n");
         echo ("					        <td>\n");
         echo ("					         " . $trunksjson->trunks[$x]->name . "\n");
