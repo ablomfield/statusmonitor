7c040dfacccd9676685476f7bbe1e85765c63d2f
Adam Blomfield <adam@blomfield.me>
2025-07-23 10:04:50 -0500

    lgw

--- html/types/lgw.php
+++ html/types/lgw.php
@@ -1,5 +1,4 @@
 <?php
-echo ("			<h1>Local Gateways</h1><br>\n");
 // Retrieve Trunk List
 $trunksurl = "https://webexapis.com/v1/telephony/config/premisePstn/trunks?orgId=$orgid";
 $gettrunks = curl_init($trunksurl);
@@ -7,37 +6,30 @@ curl_setopt($gettrunks, CURLOPT_CUSTOMREQUEST, "GET");
 curl_setopt($gettrunks, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($gettrunks, CURLOPT_FAILONERROR, true);
 curl_setopt(
-  $gettrunks,
-  CURLOPT_HTTPHEADER,
-  array(
-    'Content-Type: application/json',
-    'Authorization: Bearer ' . $authtoken
-  )
+    $gettrunks,
+    CURLOPT_HTTPHEADER,
+    array(
+        'Content-Type: application/json',
+        'Authorization: Bearer ' . $authtoken
+    )
 );
 $trunksdata = curl_exec($gettrunks);
 if (curl_errno($gettrunks) == "0") {
-  $trunksjson = json_decode($trunksdata);
-  $trunksarray = json_decode($trunksdata, true);
-  $trunkcount = count($trunksarray['trunks']);
-  echo ("					  <p>Found " . $trunkcount . " trunk(s) for $orgname</p>\n");
-  echo ("					    <table class=\"default\">\n");
-  for ($x = 0; $x < $trunkcount; $x++) {
-    if ($trunksjson->trunks[$x]->trunkType == "REGISTERING") {
-      echo ("					      <tr>\n");
-      echo ("					        <td>\n");
-      echo ("     					    <label class=\"radio-container\">\n");
-      echo ("			     		        <input type=\"radio\" name=\"trunkid\" value=\"" . $trunksjson->trunks[$x]->id . "\">\n");
-      echo ("					            <span class=\"radio-checkmark\"></span>\n");
-      echo ("					        </td>\n");
-      echo ("					        <td>\n");
-      echo ("					         " . $trunksjson->trunks[$x]->name . "</label>\n");
-      echo ("					        </td>\n");
-      echo ("					      </tr>\n");
+    $trunksjson = json_decode($trunksdata);
+    $trunksarray = json_decode($trunksdata, true);
+    $trunkcount = count($trunksarray['trunks']);
+    echo ("					    <table class=\"default\">\n");
+    for ($x = 0; $x < $trunkcount; $x++) {
+        echo ("					      <tr>\n");
+        echo ("					        <td>\n");
+        echo ("     					    <img src=\"/images/online.png\">\n");
+        echo ("					        </td>\n");
+        echo ("					        <td>\n");
+        echo ("					         " . $trunksjson->trunks[$x]->name . "\n");
+        echo ("					        </td>\n");
+        echo ("					      </tr>\n");
     }
-  }
-  echo ("					    </table>\n");
+    echo ("					    </table>\n");
 } else {
-  echo "					  <p>Sorry, no local gateways found.</p>\n";
+    echo "					  <p>Sorry, no local gateways found.</p>\n";
 }
-
-?>
